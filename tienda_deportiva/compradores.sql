8c7d3c4c76ea684cbd8830f5425db55d03b19e4b
DanielaSierra1 <sierraruizdaniela@gmail.com>
2024-10-22 22:03:51 -0500

    Añadiendo tablas sql

---
+++ tienda_deportiva/compradores.sql
@@ -0,0 +1,6 @@
+CREATE TABLE compradores (
+    id_comprador INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
